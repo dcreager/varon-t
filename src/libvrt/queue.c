d2e7570dc55fe25ae2d86dddb211cc027294511f
Douglas Creager <douglas.creager@redjack.com>
2012-05-21 09:07:29 -0400

    Refactor source code
    This patch separates the yield strategy implementations into a separate
    file.

--- src/libvrt/queue.c
+++ src/libvrt/queue.c
@@ -8,16 +8,14 @@
  * ----------------------------------------------------------------------
  */
 
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
 #include <libcork/core.h>
+#include <libcork/ds.h>
 #include <libcork/helpers/errors.h>
 
 #include "vrt/atomic.h"
 #include "vrt/error.h"
 #include "vrt/queue.h"
+#include "vrt/yield.h"
 
 
 #ifndef VRT_DEBUG_QUEUE
@@ -35,170 +33,6 @@
 #define DEFAULT_QUEUE_SIZE  65536
 #define DEFAULT_BATCH_SIZE  4096
 #define DEFAULT_STARTING_VALUE  (INT_MAX - 2*DEFAULT_BATCH_SIZE)
-#define SPIN_COUNT_BEFORE_YIELDING  100
-
-
-#if defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
-#define PAUSE() \
-        __asm__ __volatile__ ("pause");
-#else
-#define PAUSE()  /* do nothing */
-#endif
-
-
-#if defined(__APPLE__)
-#include <pthread.h>
-#define THREAD_YIELD   pthread_yield_np
-#elif defined(__linux__)
-#include <sched.h>
-#define THREAD_YIELD   sched_yield
-#else
-#error "Unknown hybrid yield implementation"
-#endif
-
-
-/*-----------------------------------------------------------------------
- * Thread yielding strategy
- */
-
-struct vrt_thread_yield_strategy {
-    struct vrt_yield_strategy  parent;
-    int  counter;
-};
-
-static void
-vrt_thread_yield_free(struct vrt_yield_strategy *vys)
-{
-    struct vrt_thread_yield_strategy  *ys =
-        cork_container_of(vys, struct vrt_thread_yield_strategy, parent);
-    free(ys);
-}
-
-static int
-vrt_thread_yield(struct vrt_yield_strategy *vys, bool first,
-                     const char *queue_name, const char *name)
-{
-    struct vrt_thread_yield_strategy  *ys =
-        cork_container_of(vys, struct vrt_thread_yield_strategy, parent);
-
-    if (first) {
-        ys->counter = SPIN_COUNT_BEFORE_YIELDING;
-    } else {
-        if (ys->counter == 0) {
-            DEBUG("[%s] %s: Yielding to other threads\n", queue_name, name);
-            THREAD_YIELD();
-        } else {
-            ys->counter--;
-            PAUSE();
-        }
-    }
-
-    return 0;
-}
-
-struct vrt_yield_strategy *
-vrt_yield_strategy_threaded(void)
-{
-    struct vrt_thread_yield_strategy  *vs =
-        cork_new(struct vrt_thread_yield_strategy);
-    vs->parent.yield = vrt_thread_yield;
-    vs->parent.free = vrt_thread_yield_free;
-    return &vs->parent;
-}
-
-
-/*-----------------------------------------------------------------------
- * Spin-wait yielding strategy
- */
-
-static int
-vrt_spin_wait_yield(struct vrt_yield_strategy *vys, bool first,
-                        const char *queue_name, const char *name)
-{
-    /* For a spin-wait, we just immediately return and let the
-     * producer/consumer try again. */
-    PAUSE();
-    return 0;
-}
-
-static void
-vrt_spin_wait_free(struct vrt_yield_strategy *vys)
-{
-    /* No-op; this is a static object */
-}
-
-static const struct vrt_yield_strategy  vrt_spin_wait_strategy = {
-    vrt_spin_wait_yield,
-    vrt_spin_wait_free
-};
-
-struct vrt_yield_strategy *
-vrt_yield_strategy_spin_wait(void)
-{
-    return (struct vrt_yield_strategy *) &vrt_spin_wait_strategy;
-}
-
-
-/*-----------------------------------------------------------------------
- * Hybrid yielding strategy
- */
-
-struct vrt_hybrid_yield_strategy {
-    struct vrt_yield_strategy  parent;
-    int  counter;
-};
-
-static void
-vrt_hybrid_yield_free(struct vrt_yield_strategy *vys)
-{
-    struct vrt_hybrid_yield_strategy  *ys =
-        cork_container_of(vys, struct vrt_hybrid_yield_strategy, parent);
-    free(ys);
-}
-
-static int
-vrt_hybrid_yield(struct vrt_yield_strategy *vys, bool first,
-                     const char *queue_name, const char *name)
-{
-    /* Adapted from
-     * http://www.1024cores.net/home/lock-free-algorithms/tricks/spinning */
-    struct vrt_hybrid_yield_strategy  *ys =
-        cork_container_of(vys, struct vrt_hybrid_yield_strategy, parent);
-
-    if (first) {
-        ys->counter = 0;
-    } else if (ys->counter < 10) {
-        /* Spin-wait */
-        PAUSE();
-    } else if (ys->counter < 20) {
-        /* A more intense spin-wait */
-        int  i;
-        for (i = 0; i < 50; i++) {
-            PAUSE();
-        }
-    } else if (ys->counter < 22) {
-        THREAD_YIELD();
-    } else if (ys->counter < 24) {
-        usleep(0);
-    } else if (ys->counter < 26) {
-        usleep(1);
-    } else {
-        usleep((ys->counter - 25) * 10);
-    }
-
-    ys->counter++;
-    return 0;
-}
-
-struct vrt_yield_strategy *
-vrt_yield_strategy_hybrid(void)
-{
-    struct vrt_hybrid_yield_strategy  *vs =
-        cork_new(struct vrt_hybrid_yield_strategy);
-    vs->parent.yield = vrt_hybrid_yield;
-    vs->parent.free = vrt_hybrid_yield_free;
-    return &vs->parent;
-}
 
 
 /*-----------------------------------------------------------------------
@@ -222,9 +56,8 @@ min_power_of_2(unsigned int in)
 
 
 struct vrt_queue *
-vrt_queue_new(const char *name,
-                  const struct vrt_value_type *value_type,
-                  unsigned int size)
+vrt_queue_new(const char *name, const struct vrt_value_type *value_type,
+              unsigned int size)
 {
     struct vrt_queue  *q = cork_new(struct vrt_queue);
     memset(q, 0, sizeof(struct vrt_queue));
@@ -353,8 +186,7 @@ vrt_wait_for_slot(struct vrt_queue *q, struct vrt_producer *p)
 
 
 static int
-vrt_claim_single_threaded(struct vrt_queue *q,
-                              struct vrt_producer *p)
+vrt_claim_single_threaded(struct vrt_queue *q, struct vrt_producer *p)
 {
     /* If there's only a single producer, we can just grab the next
      * batch of values in sequence. */
@@ -374,8 +206,7 @@ vrt_claim_single_threaded(struct vrt_queue *q,
 }
 
 static int
-vrt_claim_multi_threaded(struct vrt_queue *q,
-                             struct vrt_producer *p)
+vrt_claim_multi_threaded(struct vrt_queue *q, struct vrt_producer *p)
 {
     /* If there are multiple producerwe have to use an atomic
      * increment to claim the next batch of records. */
@@ -396,9 +227,8 @@ vrt_claim_multi_threaded(struct vrt_queue *q,
 }
 
 static int
-vrt_publish_single_threaded(struct vrt_queue *q,
-                                struct vrt_producer *p,
-                                vrt_value_id last_published_id)
+vrt_publish_single_threaded(struct vrt_queue *q, struct vrt_producer *p,
+                            vrt_value_id last_published_id)
 {
     /* If there's only a single producer, we can just update the queue's
      * cursor.  We don't have to wait for anything, because the claim
@@ -411,9 +241,8 @@ vrt_publish_single_threaded(struct vrt_queue *q,
 }
 
 static int
-vrt_publish_multi_threaded(struct vrt_queue *q,
-                               struct vrt_producer *p,
-                               vrt_value_id last_published_id)
+vrt_publish_multi_threaded(struct vrt_queue *q, struct vrt_producer *p,
+                           vrt_value_id last_published_id)
 {
     /* If there are multiple publisherthen we have to wait until all
      * of the values before the chunk that we claimed have been
@@ -488,7 +317,7 @@ vrt_queue_add_consumer(struct vrt_queue *q, struct vrt_consumer *c)
 
 struct vrt_producer *
 vrt_producer_new(const char *name, unsigned int batch_size,
-                     struct vrt_queue *q)
+                 struct vrt_queue *q)
 {
     struct vrt_producer  *p = cork_new(struct vrt_producer);
     memset(p, 0, sizeof(struct vrt_producer));
